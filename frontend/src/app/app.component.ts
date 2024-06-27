bcd74c5e4cad0f41844cbd8dd098a8fb29124d59
Jose Villalta <josevillalta@vimasistem.com>
2024-06-27 17:38:03 -0500

    Front UI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+	title = 'frontend';
+
+	customers: any[] = new Array(5);
+	services: any[] = new Array(5);
+
+	selectedCustomer: any = null;
+
+	selectCustomer(customer: any) {
+		this.selectedCustomer = customer ?? 1;
+	}
 }
